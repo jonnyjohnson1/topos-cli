10d28da24548f5543ecd333aaf9c942b85cbf72b
jonny <32085184+jonnyjohnson1@users.noreply.github.com>
2024-07-08 13:13:45 -0500

    quick toml update

--- topos/cli.py
+++ topos/cli.py
@@ -4,7 +4,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="CLI for my script")
-    parser.add_argument('command', choices=['run', 'set', 'chat'], help="Command to execute")
+    parser.add_argument('command', choices=['run', 'set', 'chat', 'zrok'], help="Command to execute")
     parser.add_argument('--web', action='store_true', help="Flag to run the server for web access")
     parser.add_argument('--local', action='store_true', help="Flag to run the server for local access (default)")
     parser.add_argument('--spacy', choices=['small', 'med', 'large', 'trf'], help="Specify Spacy model size (only for 'set' command)")
@@ -29,6 +29,16 @@ def main():
         # import chat_api
         from .chat_api import api
         api.start_chat()
+    
+    if args.command == 'zrok':
+        """
+        start the topos api chat server for clients to connect
+        """
+        # start zrok server
+        import subprocess
+        # zrok share public http://0.0.0.0:13341 # the cli command
+        subprocess.run(['zrok', 'share', 'public', 'http://0.0.0.0:13341'], check=True)
+        
     
     elif args.command == 'set':
         """
