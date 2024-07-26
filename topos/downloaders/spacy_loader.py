6e72052031b18e3d924dd23859d69d50d33caef1
jonny <32085184+jonnyjohnson1@users.noreply.github.com>
2024-07-26 12:31:59 -0500

    improved spacy downloader/config loader

--- topos/downloaders/spacy_loader.py
+++ topos/downloaders/spacy_loader.py
@@ -1,6 +1,7 @@
 import subprocess
 import yaml
-from ..utilities.utils import get_python_command
+import os
+from ..utilities.utils import get_python_command, get_root_directory
 
 
 def download_spacy_model(model_selection):
@@ -14,11 +15,15 @@ def download_spacy_model(model_selection):
         model_name = "en_core_web_trf"
     else: #default
         model_name = "en_core_web_sm"
+        
     python_command = get_python_command()
+    
+    # Define the path to the config.yaml file
+    config_path = os.path.join(get_root_directory(), 'config.yaml')
     try:
         subprocess.run([python_command, '-m', 'spacy', 'download', model_name], check=True)
         # Write updated settings to YAML file
-        with open('config.yaml', 'w') as file:
+        with open(config_path, 'w') as file:
             yaml.dump({'active_spacy_model': model_name}, file)
         print(f"Successfully downloaded '{model_name}' spaCy model.")
         print(f"'{model_name}' set as active model.")
