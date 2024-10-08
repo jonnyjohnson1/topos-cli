4dbfdc6e614d40c9eb1f53e349685f2254c718ee
luc <luc@forgotten.name>
2024-10-08 14:20:54 -0400

    feat: Add Nix flake and custom overlays (WIP)
    This commit introduces a Nix flake configuration and custom overlays for
    pyobjc-core and pystray packages. The changes include:
    
    - Add flake.nix with configuration for aarch64-darwin system
    - Create flake.lock file with pinned dependencies
    - Add custom overlay for pyobjc-core in overlays/pyobjc-core/
      - Include build.nix and default.nix for pyobjc-core
    - Add custom overlay for pystray in overlays/pystray/
      - Include default.nix for pystray
    
    NOTE: The Nix flake is currently not working due to a missing dependency.
    The `pyobjc-framework-quartz` package is not yet packaged, which prevents
    the successful building of the development environment.
    
    Error message:
      pyobjc-framework-quartz not installed
    
    These changes are a work in progress towards allowing reproducible builds
    and development environments for the project, specifically targeting
    macOS ARM64 architecture.
    
    The custom overlays address compatibility issues and provide necessary
    modifications for building pyobjc-core and pystray on the target system.
    Further work is needed to package the missing dependency or find an
    alternative solution.

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "topos";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachSystem ["aarch64-darwin"] (system:
+      let
+        pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+            poetry2nix.overlays.default
+            (final: prev: {
+                myapp = final.callPackage myapp { };
+                pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+                  (python-final: python-prev: {
+                    pyobjc-core = python-final.callPackage ./overlays/pyobjc-core/default.nix { };
+                    pystray = python-final.callPackage ./overlays/pystray/default.nix {
+                            inherit (python-final) pyobjc-core;
+                          };
+                  })
+                ];
+            })
+            ];
+        };
+
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        myapp = { poetry2nix, lib }: poetry2nix.mkPoetryApplication {
+          projectDir = self;
+          preferWheels = true;
+          overrides = poetry2nix.overrides.withDefaults (final: super:
+            lib.mapAttrs
+              (attr: systems: super.${attr}.overridePythonAttrs
+                (old: {
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
+                }))
+              {
+                # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
+                # package = [ "setuptools" ];
+              }
+          );
+        };
+
+      in
+      {
+        packages.default = pkgs.myapp;
+        devShells = {
+          # Shell for app dependencies.
+          #
+          #     nix develop
+          #
+          # Use this shell for developing your app.
+          default = pkgs.mkShell {
+            inputsFrom = [ pkgs.myapp ];
+          };
+
+          # Shell for poetry.
+          #
+          #     nix develop .#poetry
+          #
+          # Use this shell for changes to pyproject.toml and poetry.lock.
+          poetry = pkgs.mkShell {
+            packages = [ pkgs.poetry ];
+          };
+        };
+        legacyPackages = pkgs;
+      }
+    );
+}
