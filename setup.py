fa829f04c5b71f7b83b39f0a238112db6ee968e0
jonny <32085184+jonnyjohnson1@users.noreply.github.com>
2024-11-10 13:37:08 -0600

    another install test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='topos',
-    version='0.2.5',
+    version='0.2.6',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
